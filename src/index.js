5e7e999d5d8a0134f72f03c6d70c1ebd16e46a0a
Dmitry <a1@macbook-air-1.local>
2023-09-21 22:19:46 +0300

    feat: add added working code

--- src/index.js
+++ src/index.js
@@ -35,11 +35,47 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+const ass = {
+    '10': '.',
+    '11': '-'
 }
+
+function decode(expr) {
+    const arrayOfString = expr.match(/(.{1,10})/gim);
+    const reformattedByte = [];
+
+    for (let i = 0; i < arrayOfString.length ; ++i) {
+        const subArr = arrayOfString[i]
+
+        if (subArr === '**********') {
+            reformattedByte.splice(i, 1, ' ');
+        } else {
+            for (let j = 0; j < subArr.length; j = j + 2) {
+                const a = subArr[j] + subArr[j+1]
+
+                if (a > 0) {
+                    if (reformattedByte[i]) {
+                        reformattedByte.splice(i, 1, reformattedByte[i]+ass[a])
+                    } else {
+                        reformattedByte.splice(i, 1, ass[a])
+                    }
+                }
+            }
+        }
+    }
+
+    let decodedMessage = ''
+    for (let j = 0; j < reformattedByte.length; j++) {
+        if (MORSE_TABLE[reformattedByte[j]]) {
+            decodedMessage += MORSE_TABLE[reformattedByte[j]];
+        }
+    }
+
+    return decodedMessage;
+};
 
 module.exports = {
     decode
